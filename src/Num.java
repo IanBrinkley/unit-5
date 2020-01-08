ee97fd5ccf1f3f0559390cbe7565a60ea6b1eb95
IanBrinkley <54950958+IanBrinkley@users.noreply.github.com>
2020-01-08 08:56:54 -0800

    first commit with gitignore

---
+++ src/Num.java
@@ -0,0 +1,18 @@
+public class Num {
+    int value;
+    public Num (int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return value + "";
+    }
+}
---
+++ src/NumTester.java
@@ -0,0 +1,6 @@
+public class NumTester {
+    public static void main(String[] args) {
+        Num num1 = new Num(5);
+        System.out.println(num1);
+    }
+}
